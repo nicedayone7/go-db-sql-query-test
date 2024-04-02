58a6e7189e6e4d65c6f8fab0229f4ea5d7be9d76
Roman Semenkov <nicedayone7@gmail.com>
2024-04-02 22:27:00 +0300

    Add test Test_SelectClient_WhenOk

--- main_test.go
+++ main_test.go
@@ -1,49 +1,80 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	_ "modernc.org/sqlite"
 )
 
 func Test_SelectClient_WhenOk(t *testing.T) {
-	// настройте подключение к БД
+	db, err := sql.Open("sqlite", "demo.db")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer db.Close()
 
 	clientID := 1
+	client := Client{}
+	row := db.QueryRow("SELECT * FROM clients WHERE id = :id", sql.Named("id", clientID))
+	err = row.Scan(&client.ID, &client.FIO, &client.Login, &client.Birthday, &client.Email)
+	if err != nil {
+		fmt.Println(err)
+        return
+	}
 
-	// напиши тест здесь
+	assert.Equal(t, clientID, client.ID)
+	assert.NotEmpty(t, client.FIO, client.Login, client.Birthday, client.Email)
 }
 
 func Test_SelectClient_WhenNoClient(t *testing.T) {
-	// настройте подключение к БД
+	db, err := sql.Open("sqlite", "demo.db")// настройте подключение к БД
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer db.Close()
 
-	clientID := -1
+	// clientID := -1
 
 	// напиши тест здесь
 }
 
 func Test_InsertClient_ThenSelectAndCheck(t *testing.T) {
-	// настройте подключение к БД
-
-	cl := Client{
-		FIO:      "Test",
-		Login:    "Test",
-		Birthday: "19700101",
-		Email:    "mail@mail.com",
+	db, err := sql.Open("sqlite", "demo.db")// настройте подключение к БД
+	if err != nil {
+		fmt.Println(err)
+		return
 	}
+	defer db.Close()
+
+	// cl := Client{
+	// 	FIO:      "Test",
+	// 	Login:    "Test",
+	// 	Birthday: "19700101",
+	// 	Email:    "mail@mail.com",
+	// }
 
 	// напиши тест здесь
 }
 
 func Test_InsertClient_DeleteClient_ThenCheck(t *testing.T) {
-	// настройте подключение к БД
-
-	cl := Client{
-		FIO:      "Test",
-		Login:    "Test",
-		Birthday: "19700101",
-		Email:    "mail@mail.com",
+	db, err := sql.Open("sqlite", "demo.db")// настройте подключение к БД
+	if err != nil {
+		fmt.Println(err)
+		return
 	}
+	defer db.Close()
+
+	// cl := Client{
+	// 	FIO:      "Test",
+	// 	Login:    "Test",
+	// 	Birthday: "19700101",
+	// 	Email:    "mail@mail.com",
+	// }
 
 	// напиши тест здесь
 }
